472cae25361a7bd2201cc4e12f7ab3436f37b2e6
Yuki Nishidate <asta18425@gmail.com>
2022-03-30 00:01:46 +0900

    add vulkan fluid

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,2 @@
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import "@fontsource/m-plus-1";
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { ChakraProvider, Container, extendTheme } from "@chakra-ui/react";
-import "@fontsource/m-plus-1";
 import Header from "../components/header";
 import Footer from "./footer";
 
